2f45b2e2bfc5013502d0fb7618fc1f6c267ec081
Jinsung1022 <57107998+Jinsung1022@users.noreply.github.com>
2019-10-30 15:09:05 -0400

    Added a canvas with scrolls. Also adjusted the size of the window.

--- playerstruct.py
+++ playerstruct.py
@@ -10,6 +10,8 @@ class Mp3player:
     def __init__(self):
         self.win = tk.Tk()
         self.frame1 = tk.Frame(self.win, bg="black")
+        self.canvas_frame = Frame(self.frame1)
+        self.canvas = Canvas(self.canvas_frame, bg='#FFFFFF', width=600, height=350, scrollregion=(0, 0, 200, 800))
         self.label1 = tk.Label(self.win, text="", fg="white", bg="black")
         self.init_struct()
 
@@ -17,11 +19,18 @@ class Mp3player:
         mixer.init(22050, -16, 2, 4096)
         self.win.title("mp3player")
         self.win.resizable(False, False)
-        self.win.geometry("500x400")
-        self.frame1.place(x=0, y=0, anchor="nw", width=500, height=400)
+        self.win.geometry("700x500")
+        self.frame1.place(x=0, y=0, anchor="nw", width=800, height=500)
+        self.canvas_frame.place(x=50, y=150, anchor="nw", width=600, height=350)
         pause_but = Button(self.win, text="pause", command=lambda: [self.pause(), pause_but.place_forget()], bg="grey",
                            fg="white")
-        pause_but.place(x=230, y=150)
+        pause_but.place(x=320, y=100)
+        vbar = Scrollbar(self.canvas_frame, orient=VERTICAL)
+        vbar.pack(side=RIGHT, fill=Y)
+        vbar.config(command=self.canvas.yview)
+        self.canvas.config(yscrollcommand=vbar.set)
+        self.canvas.pack()
+
         self.set_songs()
         self.win.mainloop()
 
@@ -31,20 +40,20 @@ class Mp3player:
         mixer.music.load(full_str)
         mixer.music.play(2)
         self.label1['text'] = "Now Playing:  " + song
-        self.label1.place(x=135, y=100)
+        self.label1.place(x=135, y=50)
 
     def set_songs(self):
         onlyfiles = [f for f in listdir(r'C:\Users\Jinsung\Downloads\downloadbash') if
                      isfile(join(r'C:\Users\Jinsung\Downloads\downloadbash', f))]
-        x_coor = -1
-        y_coor = 200
+        self.canvas['scrollregion'] = (0, 0, 200, (len(onlyfiles)) * 85)
+        y_coor = 0
         buttons = []
         i = 0
         for songs in onlyfiles:
             buttons.append(
-                tk.Button(self.frame1, text=songs, command=lambda c=songs: self.play_song(c), bg="black", fg="white",
-                          height=3, width=70))
-            buttons[i].place(x=x_coor, y=y_coor)
+                tk.Button(self.canvas, text=songs, command=lambda c=songs: self.play_song(c), bg="black", fg="white",
+                          height=3, width=85))
+            self.canvas.create_window(-1, y_coor, anchor=NW, window=buttons[i])
             y_coor += 55
             i += 1
 
@@ -52,13 +61,13 @@ class Mp3player:
         mixer.music.pause()
         resume_but = Button(self.win, text="resume", command=lambda: [self.resume(), resume_but.place_forget()],
                             bg="grey", fg="white", anchor="w")
-        resume_but.place(x=230, y=150)
+        resume_but.place(x=320, y=100)
 
     def resume(self):
         mixer.music.unpause()
         pause_but = Button(self.win, text="pause", command=lambda: [self.pause(), pause_but.place_forget()], bg="grey",
                            fg="white", anchor="w")
-        pause_but.place(x=230, y=150)
+        pause_but.place(x=320, y=100)
 
 
 if __name__ == "__main__":
