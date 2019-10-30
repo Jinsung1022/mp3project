ae29b1e99f40ffeaf6a3ec81855d14502fd08f4b
Jinsung1022 <57107998+Jinsung1022@users.noreply.github.com>
2019-10-30 16:18:31 -0400

    Node class to store the song's information

---
+++ node.py
@@ -0,0 +1,11 @@
+class Node:
+
+    def __init__(self, song, index):
+        self.song = song
+        self.index = index
+
+    def get_song(self):
+        return self.song
+
+    def get_index(self):
+        return self.index
