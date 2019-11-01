bb382e8cf23ca2dd1b1095eaa2c90d571bc8a165
Jinsung1022 <57107998+Jinsung1022@users.noreply.github.com>
2019-11-01 15:47:42 -0400

    Music plays after one another. Also attempting to create a start menu

--- playerstruct.py
+++ playerstruct.py
@@ -1,5 +1,6 @@
 from node import Node
 from pygame import mixer
+import pygame
 from os import listdir
 from os.path import isfile, join
 from tkinter import *
@@ -8,13 +9,17 @@ import tkinter as tk
 
 class Mp3player:
 
+    num_played = 0
+
     def __init__(self):
         self.win = tk.Tk()
         self.frame1 = tk.Frame(self.win, bg="black")
+        self.menu_frame = tk.Frame(self.win, bg="black")
         self.canvas_frame = Frame(self.frame1)
         self.canvas = Canvas(self.canvas_frame, bg='#FFFFFF', width=600, height=350, scrollregion=(0, 0, 200, 800))
         self.label1 = tk.Label(self.win, text="", fg="white", bg="black")
         self.index = 0
+
         self.song_list = [f for f in listdir(r'C:\Users\Jinsung\Downloads\downloadbash') if
                           isfile(join(r'C:\Users\Jinsung\Downloads\downloadbash', f))]
         self.init_struct()
@@ -25,6 +30,7 @@ class Mp3player:
         self.win.resizable(False, False)
         self.win.geometry("700x500")
         self.frame1.place(x=0, y=0, anchor="nw", width=800, height=500)
+        self.menu_frame.place(x=0, y=0, anchor="nw", width=800, height=500)
         self.canvas_frame.place(x=50, y=150, anchor="nw", width=600, height=350)
         # Pause button
         pause_but = Button(self.win, text="pause", command=lambda: [self.pause(), pause_but.place_forget()], bg="grey",
@@ -38,14 +44,19 @@ class Mp3player:
         # Rewind button
         re_but = Button(self.win, text="Replay", command=lambda: mixer.music.rewind(), bg="grey", fg="white")
         re_but.place(x=600, y=100)
+        list_but = Button(self.win, text="Songs", bg="grey", fg="white")
+        list_but.place(x=0, y=0)
+        # Back to menu button
+        menu_but = Button(self.win, text="Back to Menu", bg="grey", fg="white")
+        menu_but.place(x=0, y=0)
         # Scroll bar
         vbar = Scrollbar(self.canvas_frame, orient=VERTICAL)
         vbar.pack(side=RIGHT, fill=Y)
         vbar.config(command=self.canvas.yview)
         self.canvas.config(yscrollcommand=vbar.set)
         self.canvas.pack()
-
         self.set_songs()
+
         self.win.mainloop()
 
     def play_song(self, node):
@@ -57,6 +68,17 @@ class Mp3player:
         mixer.music.play()
         self.label1['text'] = "Now Playing:  " + node.get_song()
         self.label1.place(x=270, y=50)
+        self.queue()
+        # pygame.mixer.music.load(next_string)
+
+    def queue(self):
+        pos = pygame.mixer.music.get_pos()
+        if int(pos) == -1:
+            self.next_song()
+            # pygame.mixer.music.load(str1)
+            # pygame.mixer.music.play()
+            # self.index += 1
+        self.win.after(1, self.queue)
 
     def set_songs(self):
         self.canvas['scrollregion'] = (0, 0, 200, (len(self.song_list) - 2) * 77)
