7cc4e2ccf3e6ff7da9f019c3671792154c788278
Jinsung1022 <57107998+Jinsung1022@users.noreply.github.com>
2019-11-04 14:45:18 -0500

    User is able to choose the directory. It's information is stored in a file called dir.txt. Bug on pause button also fixed

--- playerstruct.py
+++ playerstruct.py
@@ -3,8 +3,14 @@ from pygame import mixer
 import pygame
 from os import listdir
 from os.path import isfile, join
+import os.path
 from tkinter import *
+from tkinter.filedialog import askdirectory
 import tkinter as tk
+
+
+def raise_frame(frame):
+    frame.tkraise()
 
 
 class Mp3player:
@@ -17,12 +23,35 @@ class Mp3player:
         self.menu_frame = tk.Frame(self.win, bg="black")
         self.canvas_frame = Frame(self.frame1)
         self.canvas = Canvas(self.canvas_frame, bg='#FFFFFF', width=600, height=350, scrollregion=(0, 0, 200, 800))
-        self.label1 = tk.Label(self.win, text="", fg="white", bg="black")
+        self.label1 = tk.Label(self.frame1, text="", fg="white", bg="black")
         self.index = 0
-
-        self.song_list = [f for f in listdir(r'C:\Users\Jinsung\Downloads\downloadbash') if
-                          isfile(join(r'C:\Users\Jinsung\Downloads\downloadbash', f))]
+        self.f = ""
+        self.directory = ""
+        self.set_directory()
+        self.song_list = [f for f in listdir(self.directory) if
+                          isfile(join(self.directory, f))]
+        self.resume_but = ""
+        self.st = 'p'
         self.init_struct()
+
+    def set_directory(self):
+        if os.path.exists("dir.txt"):
+            self.f = open("dir.txt", 'r')
+            self.directory = self.f.readline()
+        else:
+            self.directory = askdirectory()
+            with open("dir.txt", 'w') as self.f:
+                self.f.write(self.directory)
+
+    def change_dir(self):
+        self.directory = askdirectory()
+        self.f.close()
+        with open("dir.txt", 'w') as self.f:
+            self.f.write(self.directory)
+        self.canvas.delete("all")
+        self.song_list = [f for f in listdir(self.directory) if
+                          isfile(join(self.directory, f))]
+        self.set_songs()
 
     def init_struct(self):
         mixer.init(22050, -16, 2, 4096)
@@ -33,22 +62,28 @@ class Mp3player:
         self.menu_frame.place(x=0, y=0, anchor="nw", width=800, height=500)
         self.canvas_frame.place(x=50, y=150, anchor="nw", width=600, height=350)
         # Pause button
-        pause_but = Button(self.win, text="pause", command=lambda: [self.pause(), pause_but.place_forget()], bg="grey",
-                           fg="white")
+        pause_but = Button(self.frame1, text="pause", command=lambda: [self.pause(), pause_but.place_forget()],
+                           bg="grey", fg="white")
         pause_but.place(x=320, y=100)
         # Next & Prev button
-        next_but = Button(self.win, text="Next", command=lambda: self.next_song(), bg="grey", fg="white")
+        next_but = Button(self.frame1, text="Next", command=lambda: self.next_song(), bg="grey", fg="white")
         next_but.place(x=380, y=100)
-        prev_but = Button(self.win, text="Prev", command=lambda: self.prev_song(), bg="grey", fg="white")
+        prev_but = Button(self.frame1, text="Prev", command=lambda: self.prev_song(), bg="grey", fg="white")
         prev_but.place(x=270, y=100)
         # Rewind button
-        re_but = Button(self.win, text="Replay", command=lambda: mixer.music.rewind(), bg="grey", fg="white")
+        re_but = Button(self.frame1, text="Replay", command=lambda: mixer.music.rewind(), bg="grey", fg="white")
         re_but.place(x=600, y=100)
-        list_but = Button(self.win, text="Songs", bg="grey", fg="white")
-        list_but.place(x=0, y=0)
+        list_but = Button(self.menu_frame, text="Songs", command=lambda: raise_frame(self.frame1),
+                          bg="grey", fg="white")
+        list_but.place(x=200, y=200)
         # Back to menu button
-        menu_but = Button(self.win, text="Back to Menu", bg="grey", fg="white")
+        menu_but = Button(self.frame1, text="Back to Menu", command=lambda: raise_frame(self.menu_frame),
+                          bg="grey", fg="white")
         menu_but.place(x=0, y=0)
+        # Change directory button
+        chdir_but = Button(self.frame1, text="Change Directory", command=lambda: self.change_dir(),
+                           bg="grey", fg="white")
+        chdir_but.place(x=600, y=0)
         # Scroll bar
         vbar = Scrollbar(self.canvas_frame, orient=VERTICAL)
         vbar.pack(side=RIGHT, fill=Y)
@@ -56,11 +91,17 @@ class Mp3player:
         self.canvas.config(yscrollcommand=vbar.set)
         self.canvas.pack()
         self.set_songs()
-
+        raise_frame(self.menu_frame)
         self.win.mainloop()
 
     def play_song(self, node):
-        full_str = r'C:\Users\Jinsung\Downloads\downloadbash' + "\\" + node.get_song()
+        if self.st == 'r':
+            self.resume_but.place_forget()
+            pause_but = Button(self.win, text="pause", command=lambda: [self.pause(), pause_but.place_forget()],
+                               bg="grey",
+                               fg="white", anchor="w")
+            pause_but.place(x=320, y=100)
+        full_str = self.directory + "\\" + node.get_song()
         self.index = node.get_index()
         # mixer.pre_init(48000, -16, 2, 4096)
         mixer.music.load(full_str)
@@ -83,8 +124,8 @@ class Mp3player:
     def set_songs(self):
         self.canvas['scrollregion'] = (0, 0, 200, (len(self.song_list) - 2) * 77)
         y_coor = 0
-        buttons = []
         i = 0
+        buttons = []
         for songs in self.song_list:
             buttons.append(
                 tk.Button(self.canvas, text=songs, command=lambda c=Node(songs, i): self.play_song(c),
@@ -103,12 +144,15 @@ class Mp3player:
 
     def pause(self):
         mixer.music.pause()
-        resume_but = Button(self.win, text="resume", command=lambda: [self.resume(), resume_but.place_forget()],
-                            bg="grey", fg="white", anchor="w")
-        resume_but.place(x=320, y=100)
+        self.st = 'r'
+        self.resume_but = Button(self.win, text="resume",
+                                 command=lambda: [self.resume(), self.resume_but.place_forget()],
+                                 bg="grey", fg="white", anchor="w")
+        self.resume_but.place(x=320, y=100)
 
     def resume(self):
         mixer.music.unpause()
+        self.st = 'p'
         pause_but = Button(self.win, text="pause", command=lambda: [self.pause(), pause_but.place_forget()], bg="grey",
                            fg="white", anchor="w")
         pause_but.place(x=320, y=100)
